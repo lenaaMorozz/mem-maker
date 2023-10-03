100107de09805d70f9ca8757719fd13545a14615
lenaaMorozz <lenaamorozz@gmail.com>
2023-10-03 19:44:17 +0500

    update main

--- src/main/java/com/mer/Main.java
+++ src/main/java/com/mer/Main.java
@@ -5,6 +5,15 @@ import com.mer.factory.CommandFactory;
 
 public class Main {
     public static void main(String[] args) {
-
+        if (args.length == 0) {
+            System.out.println("You need to enter parameters\nSee 'help'");
+            return;
+        }
+        try {
+            Command command = new CommandFactory().createCommand(args);
+            command.execute(args);
+        } catch (IllegalArgumentException e) {
+            System.out.println("You need to enter parameters\nSee 'help'");
+        }
     }
 }
